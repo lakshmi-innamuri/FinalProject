4849a0eda9b4ec68c3c1cbb07cc77d6b2be41e2a
lakshmi <innamuri.lakshmi@gmail.com>
2014-11-11 17:23:51 -0500

    adding the project

---
+++ Final Project/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector3 speed = new Vector3(10, 10,0.0f);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector3 direction = new Vector3(-1, 0,0);
+	
+	private Vector3 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector3(
+			speed.x * direction.x,
+			speed.y * direction.y,0);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody.velocity = movement;
+	}
+}
---
+++ Final Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector3 speed = new Vector3(50, 50,0);
+	
+	// 2 - Store the movement
+	private Vector3 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector3(
+			speed.x * inputX,
+			speed.y * inputY,0.0f);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+
+		// 6 - Make sure we are not outside the camera bounds
+		var dist = (transform.position - Camera.main.transform.position).z;
+		
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).x;
+		
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)
+			).x;
+		
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).y;
+		
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)
+			).y;
+		
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+			transform.position.z
+			);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody.velocity = movement;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		bool damagePlayer = false;
+		
+		// Collision with enemy
+		EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+		if (enemy != null)
+		{
+			// Kill the enemy
+			HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+			if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+			
+			damagePlayer = true;
+		}
+		
+		// Damage the player
+		if (damagePlayer)
+		{
+			HealthScript playerHealth = this.GetComponent<HealthScript>();
+			if (playerHealth != null) playerHealth.Damage(1);
+		}
+	}
+}
